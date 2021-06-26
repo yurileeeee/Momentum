6093c0d18f5148ec390f34598d8d91b9c2872f81
Lee Yu Ri <64250932+Yurigella@users.noreply.github.com>
2021-06-26 19:18:00 +0900

    JS: basic concept

---
+++ note.js
@@ -0,0 +1,24 @@
+// always const, sometimes let, never var
+// undifined = 값이 정의되지 않음, null = 값이 없음
+
+// array :  const arr = [1, "a", 3...];
+// 0부터 시작
+// arr.push(), arr.pop()
+
+// object : const player = {name: "yuri", points: 10, fat: false};
+// player.name, player["name"] 로 접근 가능
+// property 변경 가능 but 선언된 object 변경 불가
+// player.istall = true 처럼 property 추가 가능
+
+// function sayHello(argument) {
+//    실행되는 부분  
+// }
+
+// object 안의 함수 선언
+// sayHello: function(argument) {
+//    실행되는 부분
+// }
+
+// typeof value --> value의 타입 확인
+// parseInt(value) --> value의 타입을 int로 바꿔줌
+// isNaN(value) --> value가 NaN인지 확인해주는 함수
